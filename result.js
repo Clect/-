034434828b53e3f8f8bd3f3c6e488a6181fa18d3
Clect <clect@foxmail.com>
2016-08-07 23:06:40 +0800

    8-7

---
+++ result.js
@@ -0,0 +1,27 @@
+var getDiseaseRisk = {
+    "state": "0",
+    "message": "",
+    "data": "",
+    "map": {
+        "DiseaseRisk": ["前列腺癌", "肺癌", "肥胖症", "痛风", "高血压病"]
+    },
+    "key": ""
+};
+var getRiskFactors = {
+    "state": "0",
+    "message": "",
+    "data": "",
+    "map": {
+        "RiskFactors": ["饮酒", "暴饮暴食", "吸烟"]
+    },
+    "key": ""
+};
+var getDisease = {
+    "state": "0",
+    "message": "",
+    "data": "",
+    "map": {
+        "Diseases": ["脂肪肝", "脑卒中", "高血压", "冠心病", "糖尿病", "外周血管病"]
+    },
+    "key": ""
+};
